ac4c2c5bfce902cc2a80768e54088d5a9ba63ad9
Anthony DeFoe <defoe.am@gmail.com>
2024-08-27 12:15:10 -0500

    Working version of the http tests

--- tests/http_test.go
+++ tests/http_test.go
@@ -17,15 +17,35 @@ func TestSet1(t *testing.T) {
 	tests := []HttpTest{
 		{
 			name: "Populate the store, pt. 1",
-			args: HttpArgs{method: "POST", endpoint: "items", key: "0", value: "Hello World!"},
+			args: HttpArgs{method: "POST", endpoint: "/items", key: "0", value: []byte(`"Hello World!"`)},
 			want: `{"key":"0","value":"Hello World!"}`,
 		},
 		{
 			name: "Populate the store, pt. 2",
-			args: HttpArgs{method: "POST", endpoint: "items", key: "1", value: "[53,58,203]"},
-			want: `{"key":"1","value":"[53,58,203]"}`,
+			args: HttpArgs{method: "POST", endpoint: "/items", key: "1", value: []byte("[53,58,203]")},
+			want: `{"key":"1","value":[53,58,203]}`,
+		},
+		{
+			name: "Get all items, pt. 1",
+			args: HttpArgs{method: "GET", endpoint: "/items", key: "", value: []byte{}},
+			want: "",
+		},
+		{
+			name: "Get item at key 0",
+			args: HttpArgs{method: "GET", endpoint: "/items/0", key: "", value: []byte{}},
+			want: `{"key":"0","value":"Hello World!"}`,
+		},
+		{
+			name: "Delete item at key 0",
+			args: HttpArgs{method: "DELETE", endpoint: "/items/0", key: "", value: []byte{}},
+			want: `{"key":"0","value":"Hello World!"}`,
+		},
+		{
+			name: "Get all items, pt. 2",
+			args: HttpArgs{method: "GET", endpoint: "/items", key: "", value: []byte{}},
+			want: "",
 		},
 	}
 
-	HandleTests(t, tests)
+	HandleHTTPTests(t, tests)
 }
--- tests/httptestlibrary.go
+++ tests/httptestlibrary.go
@@ -10,6 +10,8 @@ import (
 	"testing"
 )
 
+var url string = "http://localhost:8080"
+
 type HttpTest struct {
 	name string
 	args HttpArgs
@@ -18,31 +20,32 @@ type HttpTest struct {
 
 type HttpArgs struct {
 	method   string // GET, POST, DELETE
-	endpoint string
+	endpoint string // /items, items/:key, /items/keys, /items/values
 	key      string
-	value    string
+	value    []byte
 }
 
+// Executes a specific request defined by a singular http test
 func (test *HttpTest) ExecuteRequest() (string, error) {
 	switch test.args.method {
 	case "GET":
-		// todo
+		return getMessage(&test.args)
 	case "POST":
-		return postMessage(test.args.GetJSONString())
+		return postMessage(&test.args)
 	case "DELETE":
-		// todo
+		return deleteMessage(&test.args)
 	default:
 		return "", errors.New("invalid http method provided")
 	}
-
-	return "", nil
 }
 
+// Builds the json formatted post request body
 func (args *HttpArgs) GetJSONString() string {
-	return fmt.Sprintf(`{"key":"%s","value":"%s"}`, args.key, args.value)
+	return fmt.Sprintf(`{"key":"%s","value":%s}`, args.key, args.value)
 }
 
-func HandleTests(t *testing.T, tests []HttpTest) {
+// Public method that handles a series of http tests.
+func HandleHTTPTests(t *testing.T, tests []HttpTest) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			res, err := test.ExecuteRequest()
@@ -50,45 +53,84 @@ func HandleTests(t *testing.T, tests []HttpTest) {
 				t.Fatalf("Error %s encountered while executing test %s", err, test.name)
 			}
 
-			if res != test.want {
+			// If want is not empty, and response is not equal to want
+			if test.want != "" && res != test.want {
 				t.Errorf("Expected %s, got %s", test.want, res)
 			}
 		})
 	}
 }
 
-// Method to interface the /items POST endpoint.
-func postMessage(jsonString string) (string, error) {
-	jsonData := []byte(jsonString)
-
-	url := "http://localhost:8080"
+// Method to interface the POST endpoint.
+func postMessage(args *HttpArgs) (string, error) {
+	jsonData := []byte(args.GetJSONString())
 
 	// Build and send the request
-	res, err := http.Post(url+"/items", "application/json", bytes.NewBuffer(jsonData))
+	res, err := http.Post(url+args.endpoint, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
 		log.Printf("Error sending request: %s", err)
 		return "", err
 	}
-	defer res.Body.Close()
 
 	// Read the response body
-	body, err := io.ReadAll(res.Body)
+	body, err := readResponseBody(res)
 	if err != nil {
-		log.Printf("Error sending request: %s", err)
+		log.Printf("Error reading response body: %s", err)
 		return "", err
 	}
 
 	return string(body), nil
 }
 
-// Method to interface the /items/ GET endpoint.
+// Method to interface the GET endpoints.
+func getMessage(args *HttpArgs) (string, error) {
+	// Make request
+	res, err := http.Get(url + args.endpoint)
+	if err != nil {
+		log.Printf("Error sending request: %s", err)
+		return "", err
+	}
 
-// Method to interface the /items/:key GET endpoint.
+	// Handle response
+	body, err := readResponseBody(res)
+	if err != nil {
+		log.Printf("Error reading response: %s", err)
+		return "", err
+	}
 
-// Method to interface the /items/keys GET endpoint.
+	return string(body), nil
+}
 
-// Method to interface the /items/values GET endpoint.
+// Method to interface the DELETE endpoints.
+func deleteMessage(args *HttpArgs) (string, error) {
 
-// Method to interface the /items DELETE endpoint.
+	// Build the request
+	req, err := http.NewRequest("DELETE", url+args.endpoint, nil)
+	if err != nil {
+		log.Printf("Error building request: %s", err)
+		return "", err
+	}
 
-// Method to interface the /items/:key DELETE endpoint.
+	// Send the request
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error sending request: %s", err)
+		return "", err
+	}
+
+	// Read response
+	body, err := readResponseBody(res)
+	if err != nil {
+		log.Printf("Error reading response body: %s", err)
+	}
+
+	return body, nil
+}
+
+// Method to read response bodies
+func readResponseBody(resp *http.Response) (body string, err error) {
+	defer resp.Body.Close()
+	temp, err := io.ReadAll(resp.Body)
+	return string(temp), err
+}
